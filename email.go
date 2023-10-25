22ad2b688d78e92bb3a7aecd87d138929aa208ed
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-Mac-mini.local>
2023-10-25 15:27:38 +0100

    adding enum for types

--- email.go
+++ email.go
@@ -13,7 +13,7 @@ type KafkaMessage struct {
 // EmailDetails encapsulates the information required to send an email.
 type EmailDetails struct {
 	// Type indicates the type of the email, e.g., WELCOME, PASSWORD_RESET, etc.
-	Type string `json:"type"`
+	Type EmailType `json:"type"`
 	// To is the recipient's email address.
 	To string `json:"to"`
 	// Username is the recipient's username.
@@ -30,3 +30,13 @@ type System struct {
 	// Metadata can contain any additional information relevant to the message context.
 	Metadata map[string]string `json:"metadata"`
 }
+
+// EmailType represents the type of email to be sent.
+type EmailType string
+
+const (
+	// WelcomeEmailType represents a welcome email.
+	WelcomeEmailType EmailType = "WELCOME"
+	// PasswordResetEmailType represents a password reset email.
+	PasswordResetEmailType EmailType = "PASSWORD_RESET"
+)
